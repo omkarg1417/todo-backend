0a298bc1ee34444ddadbdb326a8ad2bbfefe0e95
omkarg1417 <omkarg1417@gmail.com>
2021-10-14 19:17:03 +0530

    updated auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -19,12 +19,12 @@ const isLoggedIn = (req, res, next) => {
 
 const isVerified = (req, res, next) => {
     const bearerHeader = req.headers["authorization"];
-    
-    
-    if(typeof bearerHeader !== undefined) {
-        const bearerToken = bearerHeader.split(" ")[1];
-        const decoded = jwt.verify(bearerToken, SECRET);
-        const id = decoded["id"];
+    const bearerToken = bearerHeader.split(" ")[1];
+
+    const decoded = jwt.verify(bearerToken, SECRET);
+    const id = decoded["id"];
+
+    if(id) {
         req.userId = id;
         next();
     }
