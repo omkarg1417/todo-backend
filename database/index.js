7a040f3dcc13f9ce3b77e39be35551783577ae95
omkarg1417 <omkarg1417@gmail.com>
2021-10-14 19:26:56 +0530

    connected to db

--- database/index.js
+++ database/index.js
@@ -1,30 +1,76 @@
+// const {Sequelize} = require('sequelize');
+// const { sequelize_database,
+//     sequelize_username,
+//     sequelize_password,
+//     sequelize_host,
+//     sequelize_dialect, } = require("../config"); 
+
+// const sequelize = new Sequelize(
+//     sequelize_database,
+//     sequelize_username,
+//     sequelize_password,
+//     {
+//         host: sequelize_host,
+//         dialect: "postgres"
+//     }
+// )
+
+// sequelize.sync();
+
+// (async () => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log("CONNECTION ESTABLISHED WITH DB----------------------------");
+//     } catch(err) {
+//         console.log("UNABLE TO CONNECT WITH DB: " + err);
+//     }
+// })();
+
+
+// module.exports = sequelize;
+
+
 const {Sequelize} = require('sequelize');
-const { sequelize_database,
-    sequelize_username,
-    sequelize_password,
-    sequelize_host,
-    sequelize_dialect, } = require("../config"); 
 
 const sequelize = new Sequelize(
-    sequelize_database,
-    sequelize_username,
-    sequelize_password,
+    process.env.DATABASE_URL, 
     {
-        host: sequelize_host,
-        dialect: "postgres"
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
-)
+    // process.env.DATABSE_NAME,
+    // process.env.DATABASE_USERNAME,
+    // process.env.DATABASE_PASSWORD,
+    // {
+    //     host: 'localhost',
+    //     dialect: 'postgres'
+    // }
+);
 
 sequelize.sync();
 
-(async () => {
+//connecting to POSTGRES
+(async function() {
     try {
         await sequelize.authenticate();
-        console.log("CONNECTION ESTABLISHED WITH DB----------------------------");
+        console.log(`DB ${process.env.DATABSE_NAME}, CONNECTED SUCCESSFULLY`);
     } catch(err) {
-        console.log("UNABLE TO CONNECT WITH DB: " + err);
+        console.log("Unablt to connect Database");
     }
 })();
 
+// const { Client } = require('pg');
+
+// const client = new Client({
+
+// });
+
+// client.connect();
 
 module.exports = sequelize;
